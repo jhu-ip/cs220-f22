b2c94a15e96d32d5f082e10a22c2ec8655a1006b
darvishdarab <mali_dd@yahoo.com>
2022-09-15 18:37:10 -0400

    Deploy website - based on d0075164d05cfb0e555475fd6129c272c2e8a6c3

---
+++ f976f453.79d3c0d5.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[83],{143:function(e){e.exports=JSON.parse('{"pluginId":"default","version":"current","label":"Next","isLast":true,"docsSidebars":{},"permalinkToSidebar":{}}')}}]);
